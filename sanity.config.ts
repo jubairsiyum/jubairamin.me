ad2e03d931faeccac7221e03c3b54009c35312eb
jubairsiyum <jubairsiyum@gmail.com>
2025-11-17 22:41:09 +0600

    Initial Repo push

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+import { codeInput } from "@sanity/code-input";
+import { table } from "@sanity/table";
+import { projectId, dataset } from "./lib/env.api";
+import { structure } from "./sanity/structure";
+
+export default defineConfig({
+  name: "victoreke",
+  title: "jubairamin.me",
+  basePath: "/studio",
+  projectId,
+  dataset,
+  plugins: [structureTool({ structure }), visionTool(), codeInput(), table()],
+  schema: { types: schemaTypes },
+});
